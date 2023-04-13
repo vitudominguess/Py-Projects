f9e1c3ed5f724d0e22f6fdc2b6a9ee589599a34c
Vitor Pessoa Domingues <vitorpessoadomingues@hotmail.com>
2023-04-13 14:44:07 +0000

    att file

---
+++ Programming Logic/caeser_cipher/cifra de césar.py
@@ -0,0 +1,50 @@
+# Equipe: 27
+# Eduardo Pozzobom/183178/3°SI
+# Elthon Migotto/193046/3°SI
+# Matheus Sordi/182483/3°SI
+# Guilherme Nunes/192582/3°SI
+# Guilherme Ruhan/197191/1°EC
+# Vitor Pessoa/185467/3°SI
+# Kaique Cardoso/196432/1°EC
+# Joyce Barros/059339/5°SI
+# Thales Sforni/194826/1°SI
+# 12/04/2023
+
+import string
+
+def cifra(encrypt):
+    crctinvalidos = string.punctuation
+    crctvalidos = string.ascii_uppercase + string.digits
+    
+    while True:
+        mensagem = input(f"{'Encriptografar' if encrypt else 'Descriptografar'} a mensagem: ")
+        count_invalidos = [letra for letra in mensagem if letra in crctinvalidos or letra.islower()]
+        if len(count_invalidos) != 0:
+            print("Mensagem Inválida!")
+        else:
+            break
+
+    while True:
+        rot = input("Chave numérica: ")
+        if rot.isdigit() == False:
+            print("Chave inválida. Digite um valor numérico!")
+        else:
+            rot = int(rot)
+            break
+
+    mensagem_criptografada = ''
+    for i in mensagem:
+        if i in crctvalidos:
+            mensagem_criptografada += crctvalidos[(crctvalidos.index(i) + rot) % len(crctvalidos)] if encrypt else crctvalidos[(crctvalidos.index(i) - rot) % len(crctvalidos)]
+        elif i == " ":
+            mensagem_criptografada += i
+
+    print(mensagem_criptografada)
+
+while True:
+    encrypt = int(input("Escolha um número para definir o que pretende fazer:\n   1 - Encriptografar\n   0 - Descriptografar\n"))
+    if 1 != encrypt != 0:
+        print("Entrada Inválida")
+    else:
+        break
+cifra(encrypt)
